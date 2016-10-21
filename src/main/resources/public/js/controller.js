053b9b804edebeaa4cd59fe2107d4b491577421c
pjallet <philippe.jallet.external@atos.net>
2016-10-21 10:34:49 +0200

    Fix #11589 : error message when modifying community properties

--- src/main/resources/public/js/controller.js
+++ src/main/resources/public/js/controller.js
@@ -463,26 +463,23 @@ function CommunityController($scope, template, model, date, route, lang, $locati
 	$scope.updatePageTitle = function(service) {
 		var page = $scope.community.website.pages.find(function(page){ return page.titleLink === service.name; });
 		if (page) {
-            var titleCell = page.rows.first().cells.first().media.type === 'text' ?
-                page.rows.first().cells.first() :
-                page.rows.first().cells.all[1];
-			titleCell.media.source = '<h1>' + $scope.community.name + '</h1>'; // TODO : escape HTML ?
-		}
+			if(page.rows.first().cells.first().media.type === 'text'){
+				page.rows.first().cells.first().media.source = '<h1>' + $scope.community.name + '</h1>';
+			}
+			else{
+				page.rows.first().cells.first().media.source.source.community.name = $scope.community.name;
+			}		}
 	};
 
 	$scope.updatePage_home = function(service) {
 		var page = $scope.community.website.pages.find(function(page){ return page.titleLink === service.name; });
 		if (page) {
-			var titleCell = page.rows.first().cells.first().media.type === 'text' ?
-                page.rows.first().cells.first() :
-                page.rows.first().cells.all[1];
-			titleCell.media.source = '<h1>' + $scope.community.name + '</h1>'; // TODO : escape HTML ?
-			/*TEMP
-			if (page.rows.all[1].cells.all[1].media.type === 'sniplet') {
-				page.rows.all[1].cells.all[1].media = { type: 'sniplet', source: { application: 'community', template: 'message', source: { content: $scope.community.serviceHome.content } } };
+			if(page.rows.first().cells.first().media.type === 'text'){
+				page.rows.first().cells.first().media.source = '<h1>' + $scope.community.name + '</h1>';
 			}
-			TEMP*/
-		}
+			else{
+				page.rows.first().cells.first().media.source.source.community.name = $scope.community.name;
+			}		}
 	};
 
 	$scope.getPage_home = function(page, service) {
