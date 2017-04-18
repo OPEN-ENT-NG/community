ecf0870bff47db8185866e4e9263bde04c6eef18
dboissin <damien.boissin@web-education.net>
2017-04-18 15:56:24 +0200

    [Fix] #14463 : delete old communities in pages collection

--- deployment/community/migration/0.12.0-integratePages.js
+++ deployment/community/migration/0.12.0-integratePages.js
@@ -3,4 +3,5 @@ db.pages.find({"hideInPages" : true}).forEach(function(page) {
   var obj = JSON.parse(string.replace(/fr\-wseduc\-pages/g, 'net-atos-entng-community'));
   db.communityPages.save(obj);
 });
+db.pages.remove({"hideInPages" : true});
 
