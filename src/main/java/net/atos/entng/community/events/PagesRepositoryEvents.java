88e32a01fe1409c9fbb0d0c36196a4c792df5ce3
Alexandre Hascour <alexandre.hascour@web-education.net>
2018-02-16 12:15:57 +0100

    Migration vertx3

--- src/main/java/net/atos/entng/community/events/PagesRepositoryEvents.java
+++ src/main/java/net/atos/entng/community/events/PagesRepositoryEvents.java
@@ -23,8 +23,8 @@
 package net.atos.entng.community.events;
 
 import org.entcore.common.service.impl.MongoDbRepositoryEvents;
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.json.JsonArray;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
 
 public class PagesRepositoryEvents extends MongoDbRepositoryEvents {
 
