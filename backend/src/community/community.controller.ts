24b3981750326959bebe7171d1285e614196fd4c
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2025-03-31 14:14:25 +0200

    chore: rename commons to client/rest

--- backend/src/community/community.controller.ts
+++ backend/src/community/community.controller.ts
@@ -1,6 +1,7 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { CommunityService } from "./community.service";
 import { RequestLogger } from "src/logger/request-logger.service";
+import { ApiResponse } from "@nestjs/swagger";
 
 @Controller("/api/community")
 export class CommunityController {
@@ -8,4 +9,19 @@ export class CommunityController {
     private readonly communityService: CommunityService,
     private readonly logger: RequestLogger,
   ) {}
+  @Get()
+  @ApiResponse({
+    status: 200,
+    description: "List of communities with optional fields",
+    type: SearchCommunityResponseDto,
+  })
+  async findAll(
+    @Query(
+      new ValidationPipe({
+        transform: true,
+        transformOptions: { enableImplicitConversion: true },
+      }),
+    )
+    query: SearchCommunityRequestDto,
+  ) {
 }
