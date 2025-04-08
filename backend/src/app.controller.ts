50427807e0bb611828b6ba98edd2e724ced55e18
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-04-08 09:09:22 +0200

    feat: read index.html at startup

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { join } from "path";
-import { createReadStream } from "fs";
-import { Controller, Get, Res } from "@nestjs/common";
+import { createReadStream, readFileSync } from "fs";
+import { Controller, Get, OnModuleInit, Res } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { ApiExcludeEndpoint } from "@nestjs/swagger";
 import { FastifyReply } from "fastify";
@@ -8,14 +8,21 @@ import { getStaticAssetsPath } from "./config/helpers";
 
 // Todo : optimiser avec memory plutôt que createReadStream
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
+  
+  private indexHtmlContent: string;
+  
   constructor(private readonly appService: AppService) {}
+  
+  onModuleInit() {
+    // Read the file at startup and cache its content
+    const indexPath = join(getStaticAssetsPath(), "..", "index.html");
+    this.indexHtmlContent = readFileSync(indexPath, 'utf8');
+  }
+  
   @Get()
   @ApiExcludeEndpoint()
   serveIndexHtml(@Res() res: FastifyReply): void {
-    const stream = createReadStream(
-      join(getStaticAssetsPath(), "..", "index.html"),
-    );
-    res.type("text/html").send(stream);
+    res.type("text/html").send(this.indexHtmlContent);
   }
 }
