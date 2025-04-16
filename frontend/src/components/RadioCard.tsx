b5db8e49bf7a62d2df1f1e05b76bb584cc87842b
evanthonyly <anthony.ly@edifice.io>
2025-04-16 10:12:51 +0200

    feat: composant RadioCard fix

--- frontend/src/components/RadioCard.tsx
+++ frontend/src/components/RadioCard.tsx
@@ -3,55 +3,32 @@ import clsx from 'clsx';
 
 type RadioCardProps = {
   label: string;
-  value: string;
-  selectedValue: string;
-  description?: string;
   onChange: (value: string) => void;
-  model?: string;
+  selectedValue: string;
+  value: string;
   className?: string;
+  description?: string;
+  model?: string;
 };
 
-/**
- * Usage :
-  const [selected, setSelected] = useState("");
-  
-  <RadioCard
-    className="mb-16"
-    label="Pour une classe"
-    value="classe"
-    description="Recréez votre classe pour diffuser facilement des documents et ressources aux élèves, leur communiquer des informations essentielles, etc."
-    selectedValue={selected}
-    onChange={setSelected}
-  />
-  <RadioCard
-    label="Option 2"
-    value="option2"
-    description="lorem ipsum"
-    selectedValue={selected}
-    onChange={setSelected}
-  />
- */
 export default function RadioCard({
   label,
-  value,
-  selectedValue,
   onChange,
+  selectedValue,
+  value,
+  className,
   description,
-  model="",
-  className
+  model = "",
 }: RadioCardProps) {
   const isSelected = selectedValue === value;
   const classes = clsx(
-    "border py-24 border-2 rounded",
+    "border py-24 border-2 rounded-3",
     isSelected && "border-info",
     !isSelected && "border-light",
     className,
   );
   return (
-    <label
-      role="button"
-      className={classes}
-    >
+    <label role="button" className={classes}>
       <Flex justify="between" className="px-24">
         <h4 className="mb-8">{label}</h4>
         <Radio
@@ -62,7 +39,9 @@ export default function RadioCard({
           onChange={() => onChange(value)}
         />
       </Flex>
-      {description && <p className="px-24 text-gray-700 pe-32">{description}</p>}
+      {description && (
+        <p className="px-24 text-gray-700 pe-32">{description}</p>
+      )}
     </label>
   );
 }
