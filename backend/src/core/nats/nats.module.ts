db127a69760892ecc301a70839352d833d6949f5
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-04-18 15:07:45 +0200

    feat: #WB2-2701, use generated clients

--- backend/src/core/nats/nats.module.ts
+++ backend/src/core/nats/nats.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { NatsClientProvider, NATS_CLIENT } from "./nats-client.provider";
+import { EntNatsServiceClient } from "@edifice.io/edifice-ent-client";
 
 @Module({
   imports: [ConfigModule],
-  providers: [NatsClientProvider],
-  exports: [NATS_CLIENT],
+  providers: [
+    NatsClientProvider,
+    {
+      provide: EntNatsServiceClient,
+      useFactory: (natsClient) => {
+        return new EntNatsServiceClient(natsClient, "directory");
+      },
+      inject: ['NATS_CLIENT'],
+    }
+  ],
+  exports: [NATS_CLIENT, EntNatsServiceClient],
 })
 export class NatsModule {}
